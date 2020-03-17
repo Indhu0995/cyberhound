a813d1543460cce98a1e6555071b9cd9d1dfbc0d
Indhu0995 <50337288+Indhu0995@users.noreply.github.com>
2020-03-17 20:31:42 +0100

    Revert "Initial commit"
    This reverts commit 46e4dc634f45adb85293c95458b79a6e7852ffe2.

--- logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-//Logout Page, called when session gets expired or user chooses to logout
-session_start(); //starting session
-session_destroy(); //ending session
-header('Location:http://vkmbs.com/hound'); //redirecting to home page
-?>
